f3568f2da1c99058c65c40c6d4f1330fa70a4bce
BirdMilk <oenibe@gmail.com>
2023-07-13 18:38:07 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program prints out a sentence with the puts function
+ * Return: 0 (which means the program was successfully executed)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
