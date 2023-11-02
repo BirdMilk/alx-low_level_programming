c123d8d153aaf1895f328951efc9d106f76971f9
BirdMilk <oenibe@gmail.com>
2023-11-02 06:23:58 -0700

    made some corrections

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -28,11 +28,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	while (i < l1)
 	{
-		s[i] = s1[1];
+		s[i] = s1[i];
 		i++;
 	}
 
-	while (n < l2 && 1 < (l1 + n))
+	while (n < l2 && i < (l1 + n))
 		s[i++] = s2[j++];
 
 	while (n >= l2 && i < (l1 + l2))
