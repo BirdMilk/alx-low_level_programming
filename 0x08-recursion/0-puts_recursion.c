3b87dd535fc8a4cccc716a81ee063fc0fc069dc5
BirdMilk <137960932+BirdMilk@users.noreply.github.com>
2023-11-12 04:13:53 +0100

    Update 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,14 +4,13 @@
  * @s: input
  * Return: Always 0 (Success)
  */
-void _puts_recursion(char *s)
-{
-	if (*s)
-	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
 
-	else
-		_putchar('\n');
+void _puts_recursion(char *s){
+    if(*s == '\0'){
+        return;
+    }
+    _putchar(*s);
+    _puts_recursion(s + 1);
+    
+    
 }
